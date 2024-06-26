b2f25507534721713168d30c99c7065d2b5f5e4b
tmtruiner <162955777+tmtruiner@users.noreply.github.com>
2024-06-26 17:33:10 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,24 +1,55 @@
 const dropArea = document.querySelector('.drop-area');
+const fileList = document.getElementById('file-list');
+const groups = [];
+
 dropArea.classList.remove('active');
 
-["dragover", "drop"].forEach(function(event) {
-    document.addEventListener(event, function(evt) {
-    evt.preventDefault()
+["dragover", "drop"].forEach((event) => {
+    document.addEventListener(event, (evt) => {
+        evt.preventDefault()
     })
 })
 
-dropArea.addEventListener('dragenter', function() {
+dropArea.addEventListener('dragenter', () => {
     dropArea.classList.add('active')
 })
 
-dropArea.addEventListener('dragover', function() {
+dropArea.addEventListener('dragover', () => {
     dropArea.classList.add('active')
 })
 
-dropArea.addEventListener('dragleave', function() {
+dropArea.addEventListener('dragleave', () => {
     dropArea.classList.remove('active')
 })
 
-dropArea.addEventListener('drop', function() {
+dropArea.addEventListener('drop', (event) => {
     dropArea.classList.remove('active')
+    const files = event.dataTransfer.files;
+    handleFiles(files);
 })
+
+function handleFiles(files) {
+    for (let file of files) {
+        let name = file.name.toLowerCase();
+        let groupName = name.split('-')[0];
+
+        if (!groups[groupName]) {
+            groups[groupName] = [];
+        }
+
+        groups[groupName].push(name);
+        displayGroups(groups);
+    }
+}
+
+function displayGroups(groups) {
+    fileList.innerHTML = '';
+    for (const group in groups) {
+        const groupDiv = document.createElement('div');
+        groupDiv.classList.add('group');
+        groupDiv.style.display = 'inline-block';
+        groupDiv.innerHTML = `<strong>${group}</strong>: ${groups[group].length} файлов<br>${groups[group].join(', ')}`; 
+        fileList.appendChild(groupDiv);
+    }
+}
+
