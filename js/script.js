0075774ccafa973c1aa4883bf9312f97caac106e
tmtruiner <162955777+tmtruiner@users.noreply.github.com>
2024-06-27 21:57:38 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,59 +1,69 @@
 const dropArea = document.querySelector('.drop-area');
+const fileInput = document.getElementById('file-input');
 const fileList = document.getElementById('file-list');
-const groups = [];
-
-dropArea.classList.remove('active');
+const groups = new Map();
 
 ["dragover", "drop"].forEach((event) => {
     document.addEventListener(event, (evt) => {
-        evt.preventDefault()
-    })
-})
+        evt.preventDefault();
+    });
+});
 
 dropArea.addEventListener('dragenter', () => {
-    dropArea.classList.add('active')
-})
+    dropArea.classList.add('active');
+});
 
 dropArea.addEventListener('dragover', () => {
-    dropArea.classList.add('active')
-})
+    dropArea.classList.add('active');
+});
 
 dropArea.addEventListener('dragleave', () => {
-    dropArea.classList.remove('active')
-})
+    dropArea.classList.remove('active');
+});
 
 dropArea.addEventListener('drop', (event) => {
-    dropArea.classList.remove('active')
+    dropArea.classList.remove('active');
     const files = event.dataTransfer.files;
     handleFiles(files);
-})
+});
+
+fileInput.addEventListener('change', (event) => {
+    const files = event.target.files;
+    handleFiles(files);
+});
 
 function handleFiles(files) {
     for (let file of files) {
         let name = file.name;
         let groupName = name.split('-')[0];
 
-        if (!groups[groupName]) {
-            groups[groupName] = [];
+        if (!groups.has(groupName)) {
+            groups.set(groupName, []);
         }
 
-        if (groups[groupName].includes(file)) {
-            continue; 
-        }
-
-        groups[groupName].push(name);
-        displayGroups(groups);
+        groups.get(groupName).push(name);
     }
+    displayGroups(groups);
 }
 
 function displayGroups(groups) {
     fileList.innerHTML = '';
-    for (const group in groups) {
-        const groupDiv = document.createElement('div');
-        groupDiv.classList.add('group');
-        groupDiv.style.display = 'inline-block';
-        groupDiv.innerHTML = `<strong>${group}</strong>: ${groups[group].length} файлов<br>${groups[group].join(', ')}`; 
-        fileList.appendChild(groupDiv);
-    }
-}
+    for (let [group, files] of groups) {
+      let groupDiv = document.createElement('div');
+      groupDiv.classList.add('group');
+      groupDiv.style.display = 'inline-block';
+  
+      const groupHeader = document.createElement('strong');
+      groupHeader.textContent = group + ': ' + files.length + ' файлов';
+      groupDiv.appendChild(groupHeader);
+  
+      groupDiv.appendChild(document.createElement('br'));
 
+      const fileListText = document.createElement('div');
+      fileListText.textContent = files.join(' ');
+      groupDiv.appendChild(fileListText);
+      
+  
+      fileList.appendChild(groupDiv);
+    }
+  }
